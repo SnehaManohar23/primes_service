89816cf5776c755f654c4556112eb1f39aba23d9
snehamanohar23 <snehamanohar63@gmail.com>
2024-04-04 23:58:27 -0400

    changes practicum12

--- src/main/java/com/example/primesservice/controller/PrimesController.java
+++ src/main/java/com/example/primesservice/controller/PrimesController.java
@@ -1,5 +1,6 @@
 package com.example.primesservice.controller;
 
+import com.example.primesservice.rabbitmq.MQSender;
 import com.example.primesservice.service.iPrimesService;
 import org.springframework.web.bind.annotation.*;
 
@@ -8,12 +9,16 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/primes")
 public class PrimesController {
     iPrimesService primesService;
-    public PrimesController(iPrimesService primesService)
+    private final MQSender mqSender;
+    public PrimesController(iPrimesService primesService, MQSender mqSender)
     {
         this.primesService=primesService;
+        this.mqSender=mqSender;
     }
     @GetMapping("/{n}")
     public boolean isPrime(@PathVariable int n){
-        return primesService.isPrime(n);
+        boolean result=primesService.isPrime(n);
+        mqSender.sendMessage(n,result);
+        return result;
     }
 }
